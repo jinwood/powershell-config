979ea70ab0e587a0818eddd58ad3f67fec415f9a
Julian Inwood <jinw@protonmail.com>
2018-04-18 14:04:27 +0100

    update window size

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,20 @@
+﻿
 Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-a4faccd\src\posh-git.psd1'
+
 Import-Module posh-git
 $GitPromptSettings.DefaultPromptSuffix = '`n$(''>'' * ($nestedPromptLevel + 1)) '
 $GitPromptSettings.DefaultPromptPrefix = '[$(hostname)] '
 $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
 function wks { Set-Location "C:\Repositories\REPL" }
 
+$pshost = get-host
+$pswindow = $pshost.ui.rawui
+$newsize = $pswindow.buffersize
+$newsize.height = 750
+$newsize.width = 500
+$pswindow.buffersize = $newsize
+
+$newsize = $pswindow.windowsize
+$newsize.height = 35
+$newsize.width = 120
+$pswindow.windowsize = $newsize
